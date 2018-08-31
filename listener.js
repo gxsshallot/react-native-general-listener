fbf720d5c717e8d1fbad3745462f3e8204cad860
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-08-31 15:45:57 +0800

    更新下级事件监听的机制。

--- listener.js
+++ listener.js
@@ -1,7 +1,6 @@
 import { DeviceEventEmitter } from 'react-native';
 
 const rootNode = {};
-const globalNode = {};
 
 /**
  * Default seperator, you can set to modify event name connector globally.
@@ -16,31 +15,27 @@ export const innerEventType = '_##_inner_##_event_##_type_##_';
 /**
  * Register a event listener to an event type without listening to its sub event types.
  */
-export const register = _register.bind(this, false);
-
-/**
- * Register a event listener to an event type with listening to its sub event types.
- */
-export const registerWithSubEvent = _register.bind(this, true);
-
-/**
- * Register a event listener to an event type.
- * @param {boolean} includeSubEvent When a sub event triggered, is this event callback is called.
- * @param {string|array|object} type Event type, can be a string or an array of string used seperator to join or an object with json string used.
- * @param {function} func Event callback, will be called when the event is triggered.
- * @param {string} seperator A seperator to generate event name, default is defaultSeperator.
- * @returns {object} Listener object.
- */
-function _register(includeSubEvent, type, func, seperator = undefined) {
-    const eventName = convertTypeToEventName(type, seperator);
+export function register(type, func, seperator = undefined) {
+    const eventName = normalEventName(type, seperator);
     const listenerObj = DeviceEventEmitter.addListener(eventName, func);
     if (rootNode[eventName]) {
         rootNode[eventName].push(listenerObj);
     } else {
         rootNode[eventName] = [listenerObj];
     }
-    if (includeSubEvent) {
-        globalNode[eventName] = true;
+    return listenerObj;
+}
+
+/**
+ * Register a event listener to an event type with listening to its sub event types.
+ */
+export function registerWithSubEvent(type, func, seperator = undefined) {
+    const eventName = recursiveEventName(type, seperator);
+    const listenerObj = DeviceEventEmitter.addListener(eventName, func);
+    if (rootNode[eventName]) {
+        rootNode[eventName].push(listenerObj);
+    } else {
+        rootNode[eventName] = [listenerObj];
     }
     return listenerObj;
 }
@@ -52,19 +47,23 @@ function _register(includeSubEvent, type, func, seperator = undefined) {
  * @param {string} seperator A seperator to generate event name, default is defaultSeperator.
  */
 export function unregister(type, listenerObj = undefined, seperator = undefined) {
-    const eventName = convertTypeToEventName(type, seperator);
+    const eventName = normalEventName(type, seperator);
+    const rEventName = recursiveEventName(type, seperator);
     if (listenerObj) {
-        rootNode[eventName] = rootNode[eventName].filter(item => item !== listenerObj);
+        rootNode[eventName] = (rootNode[eventName] || []).filter(item => item !== listenerObj);
+        rootNode[rEventName] = (rootNode[rEventName] || []).filter(item => item !== listenerObj);
         listenerObj.remove();
-        if (rootNode[eventName].length == 0) {
+        if (rootNode[eventName] && rootNode[eventName].length == 0) {
             delete rootNode[eventName];
+        }
+        if (rootNode[rEventName] && rootNode[rEventName].length == 0) {
+            delete rootNode[rEventName];
         }
     } else {
         rootNode[eventName].forEach(obj => obj.remove());
+        rootNode[rEventName].forEach(obj => obj.remove());
         delete rootNode[eventName];
-    }
-    if (!rootNode[eventName] && globalNode[eventName]) {
-        delete globalNode[eventName];
+        delete rootNode[rEventName];
     }
 }
 
@@ -76,27 +75,44 @@ export function unregister(type, listenerObj = undefined, seperator = undefined)
  */
 export function trigger(type, state = undefined, seperator = undefined) {
     const newState = Object.prototype.isPrototypeOf(state) ? {...state, [innerEventType]: type} : state;
-    const eventName = convertTypeToEventName(type, seperator);
+    const eventName = normalEventName(type, seperator);
     DeviceEventEmitter.emit(eventName, newState);
     if (Array.isArray(type)) {
         const upperType = [...type];
         while (upperType.length > 0) {
-            upperType.pop();
-            const upperEventName = convertTypeToEventName(upperType, seperator);
-            if (globalNode[upperEventName]) {
+            const upperEventName = recursiveEventName(upperType, seperator);
+            if (rootNode[upperEventName]) {
                 DeviceEventEmitter.emit(upperEventName, newState);
             }
+            upperType.pop();
         }
     }
 }
 
 /**
- * Generate event name from an event type.
+ * Generate recursive event name from an event type.
  * @param {string|array|object} type Event type, can be a string or an array of string used seperator to join or an object with json string used.
  * @param {string} seperator A seperator to generate event name, default is defaultSeperator.
  * @returns {string} Event name.
  */
-function convertTypeToEventName(type, seperator) {
+function recursiveEventName(type, seperator) {
+    const globalHeader = '&#@!$%%$!@#&' + defaultSeperator + '1234567890987654321';
+    if (Array.isArray(type)) {
+        return globalHeader + type.join(seperator || defaultSeperator);
+    } else if (typeof type === 'string') {
+        return globalHeader + type;
+    } else {
+        return globalHeader + JSON.stringify(type);
+    }
+}
+
+/**
+ * Generate normal event name from an event type.
+ * @param {string|array|object} type Event type, can be a string or an array of string used seperator to join or an object with json string used.
+ * @param {string} seperator A seperator to generate event name, default is defaultSeperator.
+ * @returns {string} Event name.
+ */
+function normalEventName(type, seperator) {
     if (Array.isArray(type)) {
         return type.join(seperator || defaultSeperator);
     } else if (typeof type === 'string') {
