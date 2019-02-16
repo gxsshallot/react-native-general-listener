9e14f462e4a2d9ad309b4b982d37ddefd6a7b2f7
Xiaosong Gao <gaoxiaosong06@gmail.com>
2019-02-16 16:15:47 +0800

    Fix：TS声明文件的语法错误。

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,16 @@
-type EventType = string | string[] | {};
-type FunctionType = (params: {}) => void;
-type ListenerObjType = {};
+import { EmitterSubscription } from 'react-native';
 
-interface ListenerObject {
+export type EventType = string | string[] | {[key: string]: any};
+export type FunctionType = (data: any) => void;
+export type ListenerObjType = EmitterSubscription;
+
+declare var Listener: {
     defaultSeperator: string;
     innerEventType: string;
     register: (type: EventType, func: FunctionType) => ListenerObjType;
     registerWithSubEvent: (type: EventType, func: FunctionType) => ListenerObjType;
     unregister: (type: EventType, listenerObj?: ListenerObjType) => void;
-    trigger: (type: EventType, state?: {}) => void;
-}
+    trigger: (type: EventType, state?: any) => void;
+};
 
-export default const Listener: ListenerObject;
+export default Listener;
